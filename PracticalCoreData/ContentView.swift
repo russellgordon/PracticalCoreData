3d4a0f62f43f0867f5128dac5c599061e21893fd
Russell Gordon <rgordon@lcs.on.ca>
2021-07-16 09:33:02 -0400

    Initial Commit

---
+++ PracticalCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticalCoreData
+//
+//  Created by Russell Gordon on 2021-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticalCoreData/PracticalCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalCoreDataApp.swift
+//  PracticalCoreData
+//
+//  Created by Russell Gordon on 2021-07-16.
+//
+
+import SwiftUI
+
+@main
+struct PracticalCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
